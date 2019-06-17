c11cd2f9d7fdb37364aa53e39ec9f1d1201a5525
Adam Shannon <adamkshannon@gmail.com>
2019-06-17 12:24:06 -0500

    http/bind: support 'ICL' (was called x9)

--- http/bind/bind.go
+++ http/bind/bind.go
@@ -23,7 +23,8 @@ var serviceBinds = map[string]string{
 	"ach":       ":8080",
 	"auth":      ":8081",
 	"paygate":   ":8082",
-	"x9":        ":8083",
+	"x9":        ":8083", // x9 was renamed to icl
+	"icl":       ":8083",
 	"ofac":      ":8084",
 	"gl":        ":8085", // GL was renamed to accounts
 	"accounts":  ":8085",
