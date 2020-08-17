883e4628345a1f2d204c3cfdf351a2e38c8b4615
Adam Shannon <adamkshannon@gmail.com>
2020-08-17 12:57:40 -0500

    strx: add Or and Yes helper methods

---
+++ strx/strx.go
@@ -0,0 +1,30 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package strx
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Or returns the first non-empty string
+func Or(options ...string) string {
+	for i := range options {
+		if v := strings.TrimSpace(options[i]); v != "" {
+			return v
+		}
+	}
+	return ""
+}
+
+// Yes returns true if the provided case-insensitive string matches 'yes' and is used to parse config values.
+func Yes(in string) bool {
+	in = strings.TrimSpace(in)
+	if strings.EqualFold(in, "yes") {
+		return true
+	}
+	v, _ := strconv.ParseBool(in)
+	return v
+}
---
+++ strx/strx_test.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package strx
+
+import (
+	"testing"
+)
+
+func TestOr(t *testing.T) {
+	if v := Or("", "backup"); v != "backup" {
+		t.Errorf("v=%s", v)
+	}
+	if v := Or("primary", ""); v != "primary" {
+		t.Errorf("v=%s", v)
+	}
+	if v := Or("primary", "backup"); v != "primary" {
+		t.Errorf("v=%s", v)
+	}
+}
+
+func TestYes(t *testing.T) {
+	if !Yes("yes") {
+		t.Error("expected true")
+	}
+	if Yes("no") {
+		t.Error("expected no")
+	}
+
+	if !Yes("  true") {
+		t.Error("expected true")
+	}
+	if Yes("false") {
+		t.Error("expected no")
+	}
+
+	if Yes("") {
+		t.Error("expected no")
+	}
+}
