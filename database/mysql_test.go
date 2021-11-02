f3fdb7ab8fcc8c1756b5063e0aafb12134217d90
Adam Shannon <adamkshannon@gmail.com>
2021-11-02 17:31:03 -0500

    database: verify TLS connections work as expected

--- database/mysql_test.go
+++ database/mysql_test.go
@@ -46,6 +46,30 @@ func TestMySQL__basic(t *testing.T) {
 	cancelFunc()
 }
 
+func TestMySQL__TLS(t *testing.T) {
+	db := CreateTestMySQLDB(t)
+	defer db.Close()
+
+	err := db.DB.Ping()
+	require.NoError(t, err)
+
+	// create a phony MySQL
+	mysqlConfig := &MySQLConfig{
+		User:               sharedMySQLConfig.User,
+		Password:           sharedMySQLConfig.Password,
+		Address:            sharedMySQLConfig.Address,
+		UseTLS:             true,
+		InsecureSkipVerify: true,
+	}
+	m, err := mysqlConnection(log.NewNopLogger(), mysqlConfig, db.name)
+	require.NoError(t, err)
+
+	conn, err := m.Connect(context.Background())
+	require.NoError(t, err)
+
+	require.NoError(t, conn.Ping())
+}
+
 func TestMySQLUniqueViolation(t *testing.T) {
 	err := errors.New(`problem upserting depository="282f6ffcd9ba5b029afbf2b739ee826e22d9df3b", userId="f25f48968da47ef1adb5b6531a1c2197295678ce": Error 1062: Duplicate entry '282f6ffcd9ba5b029afbf2b739ee826e22d9df3b' for key 'PRIMARY'`)
 	if !UniqueViolation(err) {
