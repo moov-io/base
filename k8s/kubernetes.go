a3f6b0f88e9cd3319959d1f7d225f9405e7c168f
Adam Shannon <adamkshannon@gmail.com>
2022-05-19 09:34:03 -0500

    k8s: update to check more paths

--- k8s/kubernetes.go
+++ k8s/kubernetes.go
@@ -8,14 +8,24 @@ import (
 	"os"
 )
 
+var serviceAccountFilepaths = []string{
+	// https://stackoverflow.com/a/49045575
+	"/var/run/secrets/kubernetes.io",
+
+	// https://github.com/hashicorp/vault/blob/master/command/agent/auth/kubernetes/kubernetes.go#L20
+	"/var/run/secrets/kubernetes.io/serviceaccount/token",
+}
+
 // Inside returns true if ran from inside a Kubernetes cluster.
 func Inside() bool {
-	// kubernetes service account filepath (on default config).
-	// See https://stackoverflow.com/a/49045575
-	path := "/var/run/secrets/kubernetes.io"
-	if v := os.Getenv("KUBERNETES_SERVICE_ACCOUNT_FILEPATH"); v != "" {
-		path = v
+	// Allow a user override path
+	paths := append(serviceAccountFilepaths, os.Getenv("KUBERNETES_SERVICE_ACCOUNT_FILEPATH"))
+
+	for i := range paths {
+		if _, err := os.Stat(paths[i]); err == nil {
+			return true
+		}
 	}
-	_, err := os.Stat(path)
-	return err == nil // file exists
+
+	return false
 }
