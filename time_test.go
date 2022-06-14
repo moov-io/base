efe3628a8414be83b90060750d01f28e703966cd
Morgan Hargrove <morgan.hargrove@moov.io>
2022-06-13 17:26:35 -0700

    feat: Add AddBusinessDay to Time

--- time_test.go
+++ time_test.go
@@ -265,6 +265,41 @@ func TestTime__IsWeekend(t *testing.T) {
 	}
 }
 
+func TestTime_AddBusinessDay(t *testing.T) {
+	unchangeable := time.Date(2021, time.July, 15, 1, 0, 0, 0, est)
+	tests := []struct {
+		Date   time.Time
+		Future time.Time
+		Days   int
+	}{
+		// Thursday add one day needs to be friday
+		{time.Date(2018, time.January, 11, 1, 0, 0, 0, est), time.Date(2018, time.January, 12, 1, 0, 0, 0, est), 1},
+		// Thursday add two days over a monday holiday and needs to be following tuesday
+		{time.Date(2018, time.January, 11, 1, 0, 0, 0, est), time.Date(2018, time.January, 16, 1, 0, 0, 0, est), 2},
+		// Friday add two days over a monday holiday and needs to be following wednesday
+		{time.Date(2018, time.January, 12, 1, 0, 0, 0, est), time.Date(2018, time.January, 17, 1, 0, 0, 0, est), 2},
+		// Friday add two days over a sunday public holiday (moved to monday which is business day but not banking day) needs to be following tuesday
+		{time.Date(2021, time.July, 2, 1, 0, 0, 0, est), time.Date(2021, time.July, 6, 1, 0, 0, 0, est), 2},
+		// Negative input
+		{unchangeable, unchangeable, 0},
+		{unchangeable, unchangeable, -1},
+		{unchangeable, unchangeable, -10},
+		// Input above the max
+		{unchangeable, unchangeable, 501},
+		{unchangeable, unchangeable, 600},
+		// Input at the max
+		{time.Date(2021, time.July, 2, 1, 0, 0, 0, est), time.Date(2023, time.June, 26, 1, 0, 0, 0, est), 500},
+		// Find one year in the future
+		{time.Date(2021, time.July, 2, 1, 0, 0, 0, est), time.Date(2022, time.June, 28, 1, 0, 0, 0, est), 365 - 11 - (52 * 2)},
+	}
+	for _, test := range tests {
+		actual := NewTime(test.Date).AddBusinessDay(test.Days)
+		if !actual.Equal(NewTime(test.Future)) {
+			t.Errorf("Adding %d business days: expected %s, got %s", test.Days, NewTime(test.Future), actual)
+		}
+	}
+}
+
 func TestTime_AddBankingDay(t *testing.T) {
 	unchangeable := time.Date(2021, time.July, 15, 1, 0, 0, 0, est)
 	tests := []struct {
