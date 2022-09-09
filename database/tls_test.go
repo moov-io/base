921410b52e0f41fc5bac3221bf3c2e505d86e173
Brandon Johnson <brandon@moov.io>
2022-09-09 10:45:32 -0600

    database: generate client certs in test

--- database/tls_test.go
+++ database/tls_test.go
@@ -4,18 +4,21 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/madflojo/testcerts"
 	"github.com/moov-io/base/database"
 	"github.com/moov-io/base/log"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_LoadClientCertsFromConfig(t *testing.T) {
+	err := testcerts.GenerateCertsToFile("/tmp/client_cert.pem", "/tmp/client_cert_private_key.pem")
+	require.Nil(t, err)
 
 	config := &database.MySQLConfig{
 		TLSClientCerts: []database.TLSClientCertConfig{
 			{
-				CertFilePath: filepath.Join("testdata", "client_cert.pem"),
-				KeyFilePath:  filepath.Join("testdata", "client_cert_private_key.pem"),
+				CertFilePath: filepath.Join("/", "tmp", "client_cert.pem"),
+				KeyFilePath:  filepath.Join("/", "tmp", "client_cert_private_key.pem"),
 			},
 		},
 	}
