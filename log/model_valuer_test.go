66c17dee719c7b9c80ea352fcbee7b464f10eb6d
Adam Shannon <adamkshannon@gmail.com>
2023-02-22 12:20:13 -0600

    test: verify log.Stringer works as expected

--- log/model_valuer_test.go
+++ log/model_valuer_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/moov-io/base/log"
+	"github.com/stretchr/testify/require"
 )
 
 type Item struct {
@@ -27,4 +28,28 @@ func TestValuer__String(t *testing.T) {
 
 	foo := Foo{}
 	logger.With(foo).Log("shouldn't panic")
+}
+
+type Mode int
+
+func (m Mode) String() string {
+	switch m {
+	case 1:
+		return "SANDBOX"
+	case 2:
+		return "PRODUCTION"
+	}
+	return "UNSPECIFIED"
+}
+
+func TestValuer_Stringer(t *testing.T) {
+	out, logger := log.NewBufferLogger()
+
+	m := Mode(2)
+
+	logger.With(log.Fields{
+		"mode": log.Stringer(m),
+	}).Log("log with .String() key/value pair")
+
+	require.Contains(t, out.String(), `mode=PRODUCTION`)
 }
