c0f96e145cb098682087459f361dd444113967ba
Adam Shannon <adamkshannon@gmail.com>
2023-11-03 09:12:54 -0500

    docs: godoc headers on telemetry

--- telemetry/tracer.go
+++ telemetry/tracer.go
@@ -14,10 +14,20 @@ const (
 	MaxArrayAttributes  = 10
 )
 
+// GetTracer returns a unique Tracer scoped to be used by instrumentation code
+// to trace computational workflows.
 func GetTracer(opts ...trace.TracerOption) trace.Tracer {
 	return otel.GetTracerProvider().Tracer(InstrumentationName, opts...)
 }
 
+// StartSpan will create a Span and a context containing the newly created Span.
+//
+// If the context.Context provided contains a Span then the new span will be a child span,
+// otherwise the new span will be a root span.
+//
+// OTEL recommends creating all attributes via `WithAttributes()` SpanOption when the span is created.
+//
+// Created spans MUST be ended with `.End()` and is the responsibility of callers.
 func StartSpan(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
 	if ctx == nil {
 		ctx = context.Background()
@@ -25,25 +35,27 @@ func StartSpan(ctx context.Context, spanName string, opts ...trace.SpanStartOpti
 	return GetTracer().Start(ctx, spanName, opts...)
 }
 
+// SpanFromContext returns the current Span from ctx.
+//
+// If no Span is currently set in ctx an implementation of a Span that performs no operations is returned.
 func SpanFromContext(ctx context.Context) trace.Span {
 	return trace.SpanFromContext(ctx)
 }
 
+// AddEvent adds an event the Span in `ctx` with the provided name and options.
 func AddEvent(ctx context.Context, name string, options ...trace.EventOption) {
 	SpanFromContext(ctx).AddEvent(name, options...)
 }
 
-// RecordError will record err as an exception span event for this span. It will also
-// return the err passed in.
+// RecordError will record err as an exception span event for this span. It will also return the err passed in.
 func RecordError(ctx context.Context, err error, options ...trace.EventOption) error {
 	options = append(options, trace.WithStackTrace(true))
 	SpanFromContext(ctx).RecordError(err, options...)
 	return err
 }
 
-// SetAttributes sets kv as attributes of the Span. If a key from kv
-// already exists for an attribute of the Span it will be overwritten with
-// the value contained in kv.
+// SetAttributes sets kv as attributes of the Span. If a key from kv already exists for an
+// attribute of the Span it will be overwritten with the value contained in kv.
 func SetAttributes(ctx context.Context, kv ...attribute.KeyValue) {
 	SpanFromContext(ctx).SetAttributes(kv...)
 }
