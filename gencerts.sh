112ae629b216698627f850cac88310237c2b2122
Carson Brown <carson@moov.io>
2024-09-30 11:54:39 -0400

    add ip to cert SANs

--- gencerts.sh
+++ gencerts.sh
@@ -9,6 +9,6 @@ cd testcerts
 openssl genrsa -out root.key 2048
 openssl req -new -x509 -days 365 -key root.key -subj "/C=CN/ST=GD/L=SZ/O=Moov, Inc./CN=Moov Root CA" -out root.crt
 openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/C=CN/ST=GD/L=SZ/O=Moov, Inc./CN=localhost" -out server.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -days 365 -in server.csr -CA root.crt -CAkey root.key -CAcreateserial -out server.crt
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost,IP:127.0.0.1") -days 365 -in server.csr -CA root.crt -CAkey root.key -CAcreateserial -out server.crt
 openssl req -newkey rsa:2048 -nodes -keyout client.key -subj "/C=CN/ST=GD/L=SZ/O=Moov, Inc./CN=moov" -out client.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -days 365 -in client.csr -CA root.crt -CAkey root.key -CAcreateserial -out client.crt
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost,IP:127.0.0.1") -days 365 -in client.csr -CA root.crt -CAkey root.key -CAcreateserial -out client.crt
