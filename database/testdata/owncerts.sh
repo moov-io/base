bcc444c192814def9e404403385b3dd059751106
Adam Shannon <adamkshannon@gmail.com>
2024-09-30 11:52:31 -0500

    build: try owning as root

--- database/testdata/owncerts.sh
+++ database/testdata/owncerts.sh
@@ -4,7 +4,7 @@ set -e
 chmod 600 /var/lib/postgresql/*.key
 chmod 644 /var/lib/postgresql/*.crt
 
-chown postgres:postgres /var/lib/postgresql/*.key
-chown postgres:postgres /var/lib/postgresql/*.crt
+chown root:root /var/lib/postgresql/*.key
+chown root:root /var/lib/postgresql/*.crt
 
 ls -l /var/lib/postgresql/
